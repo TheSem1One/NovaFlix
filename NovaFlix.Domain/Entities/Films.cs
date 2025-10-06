b6062b03990c21a4f962eb84af0febe9e5bb9370
TheSem1One <animehub021@gmail.com>
2025-10-06 10:41:25 +0300

    Add project files.

---
+++ NovaFlix.Domain/Entities/Films.cs
@@ -0,0 +1,35 @@
+﻿namespace NovaFlix.Domain.Entities
+{
+    public class Films
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+        
+        public string OriginalTitle { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Release { get; set; }
+
+        public TimeOnly Duration { get; set; }
+
+        public double Rating { get; set; }
+
+        public List<string> Genres { get; set; } = new List<string>();
+
+        public string Producer { get; set; }
+
+        public string Franchise { get; set; }
+
+        public string Studio { get; set; }
+
+        public int View { get; set; }
+
+        public string FilmDescription { get; set; }
+
+        public string LinkToTheOriginal { get; set; }
+
+        public string LinkToTheUkrainianversion { get; set; }
+    }
+}
---
+++ NovaFlix.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace NovaFlix.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
